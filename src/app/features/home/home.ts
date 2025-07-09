ab487170e3b48cd9e4a5cfeaddcd601065b06196
EDK367 <denilmonterroso22@gmail.com>
2025-07-09 09:47:33 -0600

    Home: cambios de color y estilos, se agrego contacts

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { About } from '../about/about';
 import { Technologies } from '../technologies/technologies';
+import { Contact } from '../contact/contact';
+import { Projects } from "../projects/projects";
 
 @Component({
   selector: 'app-home',
-  imports: [About, Technologies],
+  imports: [About, Technologies, Projects,  Contact],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
