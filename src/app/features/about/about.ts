4483418e0abdafa0b3d2de02852c81e3a8b1d5cf
EDK367 <denilmonterroso22@gmail.com>
2025-07-09 13:02:48 -0600

    Projects: se agrego el apartado de proyectos, en espera para colocar mis proyectos

--- src/app/features/about/about.ts
+++ src/app/features/about/about.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  standalone: true,
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './about.html',
-  styleUrl: './about.css'
+  styleUrls: ['./about.css']
 })
 export class About {
-
+  hover = false;
 }
--- src/app/features/projects/projects.ts
+++ src/app/features/projects/projects.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './projects.html',
   styleUrl: './projects.css'
 })
