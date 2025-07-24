62a1cfded7e6079802388539ec7a0352673299bb
EDK367 <denilmonterroso22@gmail.com>
2025-07-24 15:24:12 -0600

    Fixed: se arreglo el scroll

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected title = 'Portfolio';
+export class App implements OnInit {
+  title = 'Portfolio';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      window.scrollTo(0, 0);
+      // window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+  }
 }
