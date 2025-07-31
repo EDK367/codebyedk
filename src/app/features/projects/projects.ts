99c758f753bf0cb8be4d0dbeaaf3a73de0a290d0
EDK367 <denilmonterroso22@gmail.com>
2025-07-31 12:51:09 -0600

    Se agrego los botones de github entre otros detalles

--- src/app/features/projects/projects.ts
+++ src/app/features/projects/projects.ts
@@ -1,13 +1,24 @@
+
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { MoreButton } from '../../shared/more-button/more-button';
 
 @Component({
   selector: 'app-projects',
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, CommonModule, MoreButton],
   templateUrl: './projects.html',
   styleUrl: './projects.css'
 })
 export class Projects {
+  showMoreProjectsPopup = false;
 
+  openMoreProjectsPopup() {
+    this.showMoreProjectsPopup = true;
+  }
+
+  closeMoreProjectsPopup() {
+    this.showMoreProjectsPopup = false;
+  }
 }
